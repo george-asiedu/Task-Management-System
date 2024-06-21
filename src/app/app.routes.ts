20ae50d76aec3b3f70bce4a1cc0f894d01639d17
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-21 15:32:12 +0000

    auth routes defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from "./pages/auth/login/login.component";
+import { SignupComponent } from './pages/auth/signup/signup.component';
+import { ForgotPasswordComponent } from './pages/auth/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './pages/auth/verify-email/verify-email.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: 'login', 
+    component: LoginComponent, 
+    title: 'Task Management System | Login Page' 
+  },
+  { 
+    path: 'signup', 
+    component: SignupComponent, 
+    title: 'Task Management System | Signup Page' 
+  },
+  { 
+    path: 'forgot-password', 
+    component: ForgotPasswordComponent, 
+    title: 'Task Management System | Forgot Password Page' 
+  },
+  { 
+    path: 'verify-email', 
+    component: VerifyEmailComponent, 
+    title: 'Task Management System | verify Email Page' 
+  },
+]
