136b74f34cc283e4a0f3419b71c77b4bfb8925e5
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-21 15:36:04 +0000

    colors updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#8f53ef',
+        secondary: '#d782f9',
+        header: '#000000',
+        paraText: '#68696b',
+        lightGrey: '#ededed',
+        lightViolet: '#ede9ff',
+        brown: '#eedbca',
+        red: '#ed5c49',
+        lightRed: '#f8e5e7'
+      }
+    },
   },
   plugins: [],
 }
